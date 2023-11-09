41fa8719957a905266cc811d375f372abaaed8cc
federicomanni97 <manni_federico@libero.it>
2023-11-09 15:56:41 +0100

    dynamic navbar

--- src/components/Nav/NavbarApp.vue
+++ src/components/Nav/NavbarApp.vue
@@ -1,15 +1,6 @@
 <template>
         <ul class="d-flex list-unstyled py-5">
-            <li><a href="">Characters</a></li>
-            <li><a href="">Comics</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">Tv</a></li>
-            <li><a href="">Games</a></li>
-            <li><a href="">Collectibles</a></li>
-            <li><a href="">Videos</a></li>
-            <li><a href="">Fans</a></li>
-            <li><a href="">News</a></li>
-            <li><a href="">Shop</a></li>
+            <li v-for="navs in navBar"><a href="">{{navs}}</a></li>
         </ul>
 </template>
 
@@ -17,8 +8,6 @@
 export default {
     name: 'NavbarApp',
     data() {
-        
-
         return {
             navBar: [
             'Characters',
